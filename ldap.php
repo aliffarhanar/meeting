2f59023f81869d6197edcdced4f7a5a731fac81a
Alif Farhan Arizkia <aliffar24@gmail.com>
2017-07-03 17:47:55 +0700

    ldap change

--- ldap.php
+++ ldap.php
@@ -31,19 +31,18 @@ function info($PHP_AUTH_USER1){
  //$ldapconfig['host'] = '10.0.32.230';
  $ldapconfig['authrealm'] = 'User Intranet Telkom ND';
  if ($PHP_AUTH_USER1) {
-			$ds=@ldap_connect($ldapconfig['host']);
-			$r = @ldap_search( $ds, " ", 'uid=' . $PHP_AUTH_USER1);
+			$ds= ldap_connect($ldapconfig['host'])or die("Could not connect to {$ldapconfig['host']}");;
+			$r = ldap_search( $ds, " ", 'uid=' . $PHP_AUTH_USER1)or die ("Error in search
+        query");
 			if ($r) {
-					$result = @ldap_get_entries( $ds, $r);
+				$result = ldap_get_entries( $ds, $r);
+				return $result;
 			}
+		return false;
 	}
- return $result; 
 }
 
-echo info(910016);
-
-echo auth(910016,"Elektr02017");
-
+if (isset($_GET['test'])) echo info($_GET['test'];
 
 
 ?>
