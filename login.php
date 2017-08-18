53235d60d599615262189e7a8cb059581e755776
Alif Farhan Arizkia <aliffar24@gmail.com>
2017-08-18 14:30:02 +0700

    admin

--- login.php
+++ login.php
@@ -23,19 +23,18 @@
 		<?php
 			if(isset($_POST['login'])){
 				if($_POST['username'] == "admin" AND $_POST['password'] == "admin"){
-				// 
-				// }else{
-				// 	if ($user = $client->login($_POST['username'], $_POST['password'])) {
-				// 		$token = $client->get_oauth_token();
-				// 	if ($user->get('roles') == 'admin') {
-						$_SESSION['login_user'] = "login";
-						$_SESSION['name'] = "Admin";
-						$_SESSION['role'] = "admin";
-						$_SESSION['username'] = "admin";
-						$_SESSION['password'] = "admin";
-						$_SESSION['phone'] = "admin";
-						$_SESSION['email'] = "admin";
-						header("location:index.php");
+					$_SESSION['login_user'] = "login";
+					$_SESSION['name'] = "Admin";
+					$_SESSION['role'] = "admin";
+					$_SESSION['username'] = "admin";
+					$_SESSION['password'] = "admin";
+					$_SESSION['phone'] = "admin";
+					$_SESSION['email'] = "admin";
+					header("location:index.php");
+				}else{
+					if ($user = $client->login($_POST['username'], $_POST['password'])) {
+						$token = $client->get_oauth_token();
+					if ($user->get('roles') == 'admin') {
 
 					} elseif ($user->get('approved')!='approved') {
 							echo "<br>
