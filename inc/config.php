2657bdc7cc977cfcb8844adc7ae511e45bff19d9
Alif Farhan Arizkia <aliffar24@gmail.com>
2017-09-05 16:11:39 +0700

    approved

--- inc/config.php
+++ inc/config.php
@@ -3,5 +3,5 @@
 	usergrid_autoload('Apache\\Usergrid\\Client');
 	$client = new Apache\Usergrid\Client('https://api.nobackend.id','nobackend.meeting','meeting');
 	isset($_SESSION['token'])?$client->set_oauth_token($_SESSION['token']):'';
-	// var_dump($client);
+
 ?>
--- user-management.php
+++ user-management.php
@@ -25,7 +25,7 @@
 						<div class='row'>
 							<div class='col-md-8 col-md-offset-2'>
 								<div class='alert alert-danger' style='text-align:center;'>
-									<h4>Gagal Melakukan Approval,".$_GET['approved']." belum di approve.</h4>
+									<h4>Gagal Melakukan Approval,".$_GET['approve']." belum di approve.</h4>
 								</div>
 							</div>
 						</div>
